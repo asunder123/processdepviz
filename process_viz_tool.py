46f21c179ae71be67c3b932d3d819d56488b08e0
SUNDER <anand.sunder@capgemini.com>
2023-07-15 13:47:59 +0530

    Viz with nodes enhanced

--- process_viz_tool.py
+++ process_viz_tool.py
@@ -16,7 +16,8 @@ def get_running_processes():
         processes.append({
             'name': process.info['name'],
             'cpuUsage': process.info['cpu_percent'],
-            'memoryUsage': process.info['memory_percent']
+            'memoryUsage': process.info['memory_percent'],
+            'numCores': psutil.cpu_count(logical=False)
         })
     # Sort the processes based on CPU usage (descending)
     processes.sort(key=lambda x: x['cpuUsage'], reverse=True)
@@ -32,7 +33,8 @@ def get_subprocesses(main_process_name):
             subprocesses.append({
                 'name': f"Subprocess-{random.randint(1, 100)}",
                 'cpuUsage': process.info['cpu_percent'],
-                'memoryUsage': process.info['memory_percent']
+                'memoryUsage': process.info['memory_percent'],
+                'numCores': psutil.cpu_count(logical=False)
             })
     # Limit to a maximum of 3 subprocesses per main process
     return subprocesses[:3]
@@ -46,15 +48,13 @@ def update_network_graph():
 
         # Add tiers (main processes) with key metrics
         for process in top_5_processes:
-            G.add_node(process['name'], node_type='tier', state='R' if process['cpuUsage'] > 75 else 'G' if process['cpuUsage'] > 25 else 'B',
-                       cpuUsage=f"CPU: {process['cpuUsage']}%", memoryUsage=f"Memory: {process['memoryUsage']}%")
+            G.add_node(process['name'], node_type='tier', state='R', cpuUsage=process['cpuUsage'], memoryUsage=0, numCores=0)
 
         # Add nodes (subprocesses) with key metrics
         for process in top_5_processes:
             subprocesses = get_subprocesses(process['name'])
             for sub_process in subprocesses:
-                G.add_node(sub_process['name'], node_type='node', state='R' if sub_process['cpuUsage'] > 75 else 'G' if sub_process['cpuUsage'] > 25 else 'B',
-                           cpuUsage=f"CPU: {sub_process['cpuUsage']}%", memoryUsage=f"Memory: {sub_process['memoryUsage']}%")
+                G.add_node(sub_process['name'], node_type='node', state='G', cpuUsage=sub_process['cpuUsage'], memoryUsage=0, numCores=0)
 
         # Add edges (connections between tiers and nodes)
         for process in top_5_processes:
@@ -68,12 +68,30 @@ def update_network_graph():
         # Draw the network graph
         pos = nx.spring_layout(G)
 
-        # Map state 'R', 'G', 'B' to corresponding colors
-        color_map = {'R': 'red', 'G': 'green', 'B': 'blue'}
-        node_colors = [color_map[G.nodes[node].get('state', 'B')] for node in G.nodes]
-        node_labels = {node: G.nodes[node].get('cpuUsage', '') + '\n' + G.nodes[node].get('memoryUsage', '') for node in G.nodes() if G.nodes[node].get('node_type', '') == 'tier'}
+        # Create concentric circles for each node to represent health and metrics
+        node_labels = {}
+        node_colors = []
+        node_sizes = []
+        for node in G.nodes:
+            node_data = G.nodes[node]
+            if G.nodes[node].get('node_type', '') == 'tier':
+                color = 'red' if node_data.get('state', '') == 'R' else 'yellow' if node_data.get('state', '') == 'Y' else 'green'
+                node_colors.append(color)
+                node_sizes.append(5000)
+                label = f"{node}\nCPU: {node_data.get('cpuUsage', 0):.2f}%"
+                node_labels[node] = label
+            else:
+                color = 'red' if node_data.get('state', '') == 'R' else 'yellow' if node_data.get('state', '') == 'Y' else 'green'
+                node_colors.append(color)
+                node_sizes.append(3000)
+                label = f"{node}\nCPU: {node_data.get('cpuUsage', 0):.2f}%"
+                node_labels[node] = label
 
-        nx.draw(G, pos, with_labels=True, node_color=node_colors, node_size=2000, font_size=12, labels=node_labels, font_color='white')
+        # Draw nodes and edges
+        nx.draw(G, pos, with_labels=False, node_color=node_colors, node_size=node_sizes, font_size=10, labels=None, font_color='black', arrows=False, width=1.5, alpha=0.7)
+        
+        # Draw node labels
+        nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=12, font_color='white')
 
         # Refresh the canvas
         canvas.draw()
@@ -84,20 +102,15 @@ def update_network_graph():
 
 # Function to update the GUI
 def update_gui():
-    try:
-        update_network_graph()
-        root.after(2000, update_gui)  # Refresh every 2 seconds
-
-    except Exception as e:
-        # Log the exception with traceback and line number
-        logging.exception(f"Error in update_gui: {e}")
+    update_network_graph()
+    root.after(2000, update_gui)  # Refresh every 2 seconds
 
 # Create the Tkinter GUI
 root = tk.Tk()
 root.title("AppDynamics-like Dynamic Network Visualization")
 
 # Create a canvas to display the network graph
-figure = plt.figure(figsize=(10, 6))
+figure = plt.figure(figsize=(12, 8))
 canvas = FigureCanvasTkAgg(figure, master=root)
 canvas.get_tk_widget().pack()
 
