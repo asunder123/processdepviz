f7e08649a0e5d76b1cc9b30cf98e23283f7f367c
SUNDER <anand.sunder@capgemini.com>
2023-07-16 23:53:19 +0530

    Digital twin simulated ver 1.0

--- digital_twin.py
+++ digital_twin.py
@@ -1,6 +1,15 @@
 import pickle
 import psutil
 import random
+import tkinter as tk
+import matplotlib.pyplot as plt
+import networkx as nx
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import logging
+import matplotlib.colors as mcolors
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load the prediction model from the saved file
 def load_prediction_model():
@@ -29,19 +38,62 @@ def generate_digital_twin(process_name, cpu_usage, memory_usage, num_cores, pred
     # Other digital twin simulation tasks can be added based on the prediction and process data
 
 
-def main():
-    # Load the prediction model
-    prediction_model = load_prediction_model()
+def update_network_graph():
+    try:
+        prediction_model = load_prediction_model()
 
-    # Simulate a Windows process (replace this with actual process monitoring)
-    process_name = "ExampleProcess"
-    cpu_usage = random.uniform(0, 100)
-    memory_usage = random.uniform(0, 100)
-    num_cores = psutil.cpu_count(logical=False)
+        G = nx.DiGraph()
 
-    # Generate the digital twin based on the process data and prediction model
-    generate_digital_twin(process_name, cpu_usage, memory_usage, num_cores, prediction_model)
+        # Simulate Windows processes (replace this with actual process monitoring)
+        for i in range(5):
+            process_name = f"Process-{i}"
+            cpu_usage = random.uniform(0, 100)
+            memory_usage = random.uniform(0, 100)
+            num_cores = psutil.cpu_count(logical=False)
+
+            generate_digital_twin(process_name, cpu_usage, memory_usage, num_cores, prediction_model)
+
+            G.add_node(process_name, node_type='process', cpuUsage=cpu_usage, memoryUsage=memory_usage, numCores=num_cores)
+
+        plt.clf()
+        pos = nx.spring_layout(G)
+
+        node_colors = []
+        node_sizes = []
+        node_labels = {}
+        for node in G.nodes:
+            node_data = G.nodes[node]
+            color = get_color(node_data.get('cpuUsage', 0))
+            node_colors.append(color)
+            node_sizes.append(5000)
+            label = f"{node}\nCPU: {node_data.get('cpuUsage', 0):.2f}%\nRAM: {node_data.get('memoryUsage', 0):.2f}%\nCores: {node_data.get('numCores', 0)}"
+            node_labels[node] = label
+
+        nx.draw(G, pos, with_labels=False, node_color=node_colors, node_size=node_sizes, font_size=10, labels=None, font_color='black', arrows=False, width=1.5, alpha=0.7)
+
+        nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=6, font_color='black')
+
+        canvas.draw()
+        root.after(2000, update_network_graph)  # Refresh every 2 seconds
+
+    except Exception as e:
+        logging.exception(f"Error in update_network_graph: {e}")
 
 
-if __name__ == "__main__":
-    main()
+def get_color(cpu_usage):
+    cmap = plt.get_cmap('coolwarm')
+    norm = mcolors.Normalize(vmin=0, vmax=100)
+    color = cmap(norm(cpu_usage))
+    return color
+
+
+root = tk.Tk()
+root.title("Dynamic Network Visualization")
+
+figure = plt.figure(figsize=(12, 8))
+canvas = FigureCanvasTkAgg(figure, master=root)
+canvas.get_tk_widget().pack()
+
+update_network_graph()
+
+root.mainloop()
